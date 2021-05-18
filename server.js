397ca454e3b4920106fd0369ff35467981037bfc
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2021-05-18 14:03:16 +0530

    additional packages,config

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const dotEnv = require("dotenv");
+
+//configuration
+dotEnv.config({ path: "./configuration/config.env" });
 
 const app = express();
 //sample get
+
 app.get("/", (req, res) => {
     res.send("Testing server");
 });
 
-app.listen(5000, console.log("sever running at port 5000"));
+const port = process.env.PORT || 4000;
+
+app.listen(
+    port,
+    console.log(`server running at PORT:${port}, mode : ${process.env.NODE_ENV}`)
+);
