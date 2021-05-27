a6db030182be8922f03136b2bfb6054bb2ebbdfc
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2021-05-27 23:46:24 +0530

    userpage bug:userpage was build using booksmodel->now its on user model

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { verifyAuth, verifyGuest } = require("../middleware/auth_middleware");
 const Book = require("../mongoDB/models/Book");
+const User = require("../mongoDB/models/User");
 
 //single user page
 router.get("/:id", verifyAuth, async(req, res) => {
@@ -11,20 +12,21 @@ router.get("/:id", verifyAuth, async(req, res) => {
             })
             .populate("user")
             .lean();
-        const singlebook = await Book.findOne({
-                //to grab the user from singlebook
-                user: req.params.id,
-            })
-            .populate("user")
-            .lean();
+
+        const singleUser = await User.findOne({
+            _id: req.params.id,
+        }).lean();
+
         res.render("user", {
             books,
-            name: req.user.displayName,
-            singlebook,
+            name: req.user.displayName, //for navbar partials
+            singleUser,
         });
+        console.log(singleUser)
     } catch (error) {
         console.log(error);
         res.render("error");
+
     }
 });
 
